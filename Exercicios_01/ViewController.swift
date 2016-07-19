9eb435602e9e369ba2f9866947839b1ba6e2d90e
Diego Fonseca <diegofonseca@macbook-pro-de-diego.local>
2016-07-18 21:17:14 -0300

    Initial Commit

---
+++ Exercicios_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercicios_01
+//
+//  Created by Diego Fonseca on 18/07/16.
+//  Copyright © 2016 Diego Fonseca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
